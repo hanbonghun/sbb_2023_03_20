c1e282906f6d75dfee3185b021049df72f0744df
hanbonghun <bng4535@khu.ac.kr>
2023-03-20 23:44:25 +0900

    인증 기본 설정(임시로 모든 권한 접근 허용)

---
+++ src/main/java/com/mysite/sbb/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.mysite.sbb;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration //스프링의 환경 설정 파일임을 의미
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    //인증되지 않은 모든 요청 허용
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().requestMatchers(
+                new AntPathRequestMatcher("/**")).permitAll()
+        ;
+        return http.build();
+    }
+}
